7950b43f5ec57dde72828b30344fb70e427cfe53
lego651 <legogao651@gmail.com>
2018-10-20 15:56:31 -0700

    refractor base structure

--- Server/config.js
+++ Server/config.js
@@ -2,8 +2,7 @@ module.exports = {
     hostUrl: 'http://localhost:5000',
     clientUrl: 'http://localhost:8080',
     // database: 'mongodb://localhost:27017/iStudy',
-    // database: 'mongodb://Lego_Gao:Gby032791@ds017432.mlab.com:17432/stock_charts',
-    databse: 'mongodb://jason:Ca651651!@ds137483.mlab.com:37483/wip',
+    database: 'mongodb://Lego_Gao:Gby032791@ds017432.mlab.com:17432/stock_charts',
     secret: "$@%#@$@!%&$#&@$",
     port: 5000,
 };
